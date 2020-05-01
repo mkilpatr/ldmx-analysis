03fbb0edc14d950c880f81e1219ee0d6b2f30445
Tom E <tom.eichlersmith@gmail.com>
2020-05-01 15:15:59 -0500

    update example config

--- python/config/ana_example_config.py
+++ python/config/ana_example_config.py
@@ -2,11 +2,14 @@
 from LDMX.Framework import ldmxcfg
 
 # Create a process with pass name "recon"
-p=ldmxcfg.Process('recon')
+p=ldmxcfg.Process('vetoana')
 
 # Append the library that contains the analyzer below to the list of libraries 
 # that the framework will load.
-p.libraries.append("/path/to/libAnalysis.so")
+# You will need to add the path to your install location to PYTHONPATH, e.g.
+#   export PYTHONPATH=/path/to/this/install/lib/python:$PYTHONPATH
+from LDMXANA import libAnaPath
+p.libraries.append( libAnaPath.libAnaPath() )
 
 # Create an instance of the ECal veto analyzer.  This analyzer is used to create
 # an ntuple out of ECal BDT variables. The analyzer requires that the
@@ -21,12 +24,13 @@ hcal_veto_ana = ldmxcfg.Producer("hcal", "ldmx::HCalVetoAnalyzer")
 # Define the order in which the analyzers will be executed.
 p.sequence=[ecal_veto_ana, hcal_veto_ana]
 
-# Specify the list of input files. 
-p.inputFiles=['input_file.root']
+# input the file as an argument on the command line
+import sys
+p.inputFiles=[sys.argv[1]]
 
 # Specify the output file.  When creating ntuples or saving histograms, the 
 # output file name is specified by setting the attribute histogramFile.  
 p.histogramFile='ntuple_test.root'
 
 # Print out the details of the configured analyzers. 
-p.printMe()
+print p
